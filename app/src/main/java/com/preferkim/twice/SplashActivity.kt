48fc920ad0a889353dae5516d4854ff2b82a30de
Balparang <shineecard5@gmail.com>
2022-03-20 19:42:41 +0900

    [추가] SplashActivity 사용법(Handler, AndroidManifest.xml)

--- app/src/main/java/com/preferkim/twice/SplashActivity.kt
+++ app/src/main/java/com/preferkim/twice/SplashActivity.kt
@@ -1,12 +1,29 @@
-
 package com.preferkim.twice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // SplashActivity -> MainActivity 이동
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000) // 1초 동안 SplashActivity를 보여줌
+
     }
+
+    /*
+    onCreate메서드 바깥 쪽에서는 Handler()가 인식되지 않는다.
+     */
+//    Handler().postDelayed({
+//        startActivity(Intent(this, MainActivity::class.java))
+//        finish()
+//    }, 3000)
 }
